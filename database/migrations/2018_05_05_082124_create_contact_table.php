cf7206bd22b3a3ccae046b62c8659bcbfc303c9e
Luka1812 <luka.vavetic@gmail.com>
2018-05-05 10:50:47 +0200

    create contact and phone_number migrations

---
+++ database/migrations/2018_05_05_082124_create_contact_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname', 60);
+            $table->string('lastname', 60);
+            $table->string('email', 100)->unique();
+            $table->string('profile_photo', 100);
+            $table->unsignedTinyInteger('is_favorite')->nullable();
+            $table->timestamps();
+        });
+        
+        DB::statement('ALTER TABLE contact ADD FULLTEXT INDEX contact_fulltext (firstname, lastname, email)');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact');
+    }
+}
---
+++ database/migrations/2018_05_05_084444_create_phone_number_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhoneNumberTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phone_number', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('contact_id');
+            $table->string('number', 50);
+            $table->string('label', 150)->nullable();
+            
+            $table->foreign('contact_id')
+                ->references('id')->on('contact')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phone_number');
+    }
+}
