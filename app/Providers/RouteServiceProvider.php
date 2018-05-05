8d0d4cd4de2ae8d5c2eb0681b5f3765d5f8c4bab
Luka1812 <luka.vavetic@gmail.com>
2018-05-05 12:03:06 +0200

    prepare logic in RouteServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -15,6 +15,20 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     protected $namespace = 'App\Http\Controllers';
+    
+    /**
+     * Web namespace
+     *
+     * @var string
+     */
+    protected $namespaceWeb = 'App\Http\Controllers\Web';
+    
+    /**
+     * API namespace
+     *
+     * @var string
+     */
+    protected $namespaceApi = 'App\Http\Controllers\Api';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -39,7 +53,6 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
-        //
     }
 
     /**
@@ -52,7 +65,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapWebRoutes()
     {
         Route::middleware('web')
-             ->namespace($this->namespace)
+             ->namespace($this->namespaceWeb)
              ->group(base_path('routes/web.php'));
     }
 
@@ -67,7 +80,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::prefix('api')
              ->middleware('api')
-             ->namespace($this->namespace)
+             ->namespace($this->namespaceApi)
              ->group(base_path('routes/api.php'));
     }
 }
