4bf16f815fae17597b862f853048809382777fa2
Luka1812 <luka.vavetic@gmail.com>
2018-05-06 18:42:57 +0200

    prepare API routes for PhoneNumbers API

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,7 @@ $router->delete('contact/{id}/', 'ContactController@delete');
 
 //phone numbers
 $router->get('phone-numbers/', 'PhoneNumberController@index');
+$router->get('phone-number/{id}/', 'PhoneNumberController@show');
+$router->post('phone-number/', 'PhoneNumberController@insert');
+$router->put('phone-number/{id}/', 'PhoneNumberController@update');
+$router->delete('phone-number/{id}/', 'PhoneNumberController@delete');
