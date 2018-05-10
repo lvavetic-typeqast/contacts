759512e819c5d98adb983667387fad42cf5be27d
Luka Vavetić <luka@slink.hr>
2018-05-10 10:22:37 +0200

    fix bug in search.js script - problem with length

--- resources/assets/js/web/page/search.js
+++ resources/assets/js/web/page/search.js
@@ -20,11 +20,13 @@ var searchbox = new Vue({
             axios.post('http://www.contacts.com/api/contact_search?search=' + searchbox.search)
                 .then(function (response) {
                     console.log(response.data)
-                    if (response.data.length > 0) {
+
+                    if (response.data.data.length > 0) {
                         searchbox.currentStatus = "Data with keyword: " + searchbox.search + ' has been found!'
                     } else {
                         searchbox.currentStatus = "Data with keyword: " + searchbox.search + ' has not been found!'
                     }
+                    
                     searchbox.models = response.data
                 })
                 .catch(function(error){
