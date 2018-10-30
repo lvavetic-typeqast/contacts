8453e19a5a2df1a33e1bb3c4165b0c5c842b91c2
lvavetic-typeqast <luka.vavetic@typeqast.com>
2018-10-30 13:02:02 +0100

    cleanup webpack file

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,3 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
-
-mix.scripts('resources/assets/js/web/page/search.js', 'public/js/web/page/search.min.js');
